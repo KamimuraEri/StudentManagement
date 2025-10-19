e3c64ba94b688646acace8765be261d913965e37
Kamimura Eri <shef0323@gmail.com>
2025-10-20 05:20:28 +0900

    apache lang

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,5 +1,7 @@
 package raisetech.student.management;
 
+import ch.qos.logback.core.util.StringUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,9 @@ public class Application {
     		SpringApplication.run(Application.class, args);
 	}
 
+
   @GetMapping("/hello")
   public String hello() {
-    return "Hello, World!";
+    return " Hello! World !";
   }
 }
