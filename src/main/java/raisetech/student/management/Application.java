31bed2788d9bc1416f37e66b1366e96caad79b8a
Kamimura Eri <shef0323@gmail.com>
2025-10-22 16:33:13 +0900

    ブランチをマージ

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,5 +1,7 @@
 package raisetech.student.management;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
